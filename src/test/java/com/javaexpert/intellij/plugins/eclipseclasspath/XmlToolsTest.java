b56a0bb6a58787669e6df1b212953ed9c1d56cbd
piotrga <piotrga@b6543c5a-9a1a-0410-aa3b-6575023fe192>
2006-08-10 06:31:12 +0000

    added some tests

--- src/test/java/com/javaexpert/intellij/plugins/eclipseclasspath/XmlToolsTest.java
+++ src/test/java/com/javaexpert/intellij/plugins/eclipseclasspath/XmlToolsTest.java
@@ -1,5 +1,23 @@
 package com.javaexpert.intellij.plugins.eclipseclasspath;
 
-public class XmlToolsTest {
-    
+import junit.framework.TestCase;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class XmlToolsTest extends TestCase {
+
+    public void test() {
+
+        Document doc = XmlTools.parseXmlFile(this.getClass().getResource("test.xml").getPath(), false);
+        Element documentElement = doc.getDocumentElement();
+
+        assertNotNull(documentElement);
+
+        assertEquals("idea-plugin", documentElement.getNodeName());
+
+        assertEquals(15, documentElement.getChildNodes().getLength());
+
+        assertEquals("5581", documentElement.getChildNodes().item(9).getAttributes().getNamedItem("since-build").getNodeValue());
+
+    }
 }
