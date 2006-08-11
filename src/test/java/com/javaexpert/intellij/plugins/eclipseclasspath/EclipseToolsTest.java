7f4426137e92c1cc93cc61f756fee634d77b9f5c
piotrga <piotrga@b6543c5a-9a1a-0410-aa3b-6575023fe192>
2006-08-11 22:18:51 +0000

    added EclipseToolsTest

--- src/test/java/com/javaexpert/intellij/plugins/eclipseclasspath/EclipseToolsTest.java
+++ src/test/java/com/javaexpert/intellij/plugins/eclipseclasspath/EclipseToolsTest.java
@@ -1,10 +1,22 @@
 package com.javaexpert.intellij.plugins.eclipseclasspath;
 
+import junit.framework.TestCase;
+
+import java.util.List;
+
 /**
  * User: piotrga
  * Date: 2006-08-10
  * Time: 07:28:39
  */
-public class EclipseToolsTest {
+public class EclipseToolsTest extends TestCase {
+    public void testPersesEclipseClasspath() {
+        List<String> res = EclipseTools.parseEclipseClassPath(this.getClass().getResource(".classpath").getPath());
+        assertEquals(res.size(), 10);
+        assertTrue(res.contains("lib/cglib-nodep-2.1_3.jar"));
+        assertTrue(!res.contains("uimocks"));
+        assertTrue(!res.contains("bin"));
+        assertTrue(!res.contains("org.eclipse.jdt.launching.JRE_CONTAINER"));
 
+    }
 }
